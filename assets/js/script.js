03b76199097408e53203c3abe2565b7fa05281f4
Terry <t3rrygong@gmail.com>
2020-04-02 22:04:02 -0600

    hide search result card body before hit search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,8 @@ $(".btn").on("click", function () {
         method: "GET"
     }).then(function (response) {
         console.log(response);
+        $("#searchResult").empty();
+        $("#searchResult").parent().removeClass("d-none");
         $("#searchResult").append("<h5>" + response.name + " (" + moment().format("LL") + ")<img src='http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png' alt='weather icon'><h5/>");
         $("#searchResult").append("<p>Temperature: " + Math.round((response.main.temp - 273.15) * 9 / 5 + 32) + " " + String.fromCharCode(176) + "F<p/>");
         $("#searchResult").append("<p>Humidity: " + response.main.humidity + "%<p/>");
