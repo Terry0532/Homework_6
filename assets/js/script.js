b75ad5263b105078a9dcb863222074c6ddafba71
Terry <t3rrygong@gmail.com>
2020-04-02 21:57:59 -0600

    now I can search a city and get response and print city name, date, weather icon, temp, and humidity

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+$(".btn").on("click", function () {
+    var cityName = $("#cityName").val().trim();
+    // cityName = cityName.replace(/\s+/g, '');
+    console.log(cityName);
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?&q=" + cityName + "&appid=9726f22214b33db1ab5c46343f311c22";
+    console.log(queryURL);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        $("#searchResult").append("<h5>" + response.name + " (" + moment().format("LL") + ")<img src='http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png' alt='weather icon'><h5/>");
+        $("#searchResult").append("<p>Temperature: " + Math.round((response.main.temp - 273.15) * 9 / 5 + 32) + " " + String.fromCharCode(176) + "F<p/>");
+        $("#searchResult").append("<p>Humidity: " + response.main.humidity + "%<p/>");
+    });
+});
